5de03101a689d4f2d606bea91d93b9374534043f
Garrett Brown <Just_Garrett@protonmail.com>
2022-08-15 19:19:17 -0500

    6kyu coding challenge #33 O(sqrt(n))

--- 6kyu/is-a-number-prime.js
+++ 6kyu/is-a-number-prime.js
@@ -4,31 +4,11 @@
 
 
 function isPrime(num) {
-    let primes = [2, 3, 5, 7]
-    let count = 1
-    let test = 0
     if(num <= 1){return false}
-    if(num === 2 || num === 3){return true}
-    if(num % 2 === 0){return false}
-    while(test <= 43){
-        test = 6 * count - 1
-        for(i = 0; i < primes.length; i++){
-        if(test % primes[i] === 0){break}
-        else if(test % primes[i] != 0 && i === primes.length - 1){
-            if(test === num){return true}
-            primes.push(test)
-        }
-        }
-        test = 6 * count + 1
-        for(i = 0; i < primes.length; i++){
-        if(test % primes[i] == 0){break}
-        else if(test % primes[i] != 0 && i === primes.length - 1){
-            if(test === num){return true}
-            primes.push(test)
-        }
-        }
-        count++
+    if(num <= 3){return true}
+    if(num % 2 === 0 || num % 3 === 0){return false}
+    for(i = 5; i * i <= num; i += 6){
+      if(num % i === 0 || num % (i + 2) === 0){return false}
     }
-    if(primes.includes(num)){return true}
-    else{return false}
- }
+    return true
+}
